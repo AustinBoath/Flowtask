494bf587d4fea74b653a7e56066e4bf244de4f76
Austin <77835617+AustinBoath@users.noreply.github.com>
2021-12-13 16:33:05 +1030

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,15 @@
+import time
+
+print("Flowtask Alpha 1.0 - By Austin Boath, 2021")
+print("Start time = 1")
+print("Stop time = 0")
+start = input("Your input: ")
+
+if start == 0
+    print("You cannot stop time when no timer has started. Restart the program.")
+    ;
+if start == 1
+    worktime = worktime+1
+    sleep(1)
+    :
+input("Press 0 to stop time")
